78afb1638bc5b1af4f99cd56562152983d066351
Topher <topher@tri.be>
2021-03-21 11:44:12 -0400

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== Topher's WordPress.tv Importer ===
+Contributors: topher1kenobe
+Tags: video
+Requires at least: 5.7
+Tested up to: 5.7
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Fetches an RSS feed from WordPress.tv and injects items into the Post type.
+
+== Description ==
+
+Fetches an RSS feed from WordPress.tv and injects items into the Post type. The video is inserted into the top of the content as a shortcode. Settings are all hardcoded.  You should change:
+
+* Feed URL
+* Post type (if you want to inject into a custom post type)
+* Category IDs
+
+Please read Usage for how to make it do an import.
+
+== Installation ==
+
+1. Use the Plugin Add New menu item in WordPress.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Usage ==
+
+Go to your homepage and append this to the URL: ?wptvimport=yes
+
+I recommend you exclude ?* from your cache plugin.
+
+If you want to run this on a schedule I recommend you use an external cron and simply call this url on whatever schedule you wish.
+
+Note: this plugin keeps a unique ID for each item/post, so running the script multiple times won't make duplicates.  This also means that if you need a post updated you should delete it and re-run the import script.
+
+Note: the pubdate on the post is when the video was uploaded to WordPress.tv, NOT when the event happened, nor when the item was imported.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ topher-wptv-import.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Plugin Name: Topher's WPTV Importer
+ * Description: Grabs Topher's RSS feed from WPTV and inserts as posts
+ * Version:     1.0
+ * Author:      Topher
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: topher-wptv-import
+ * Domain Path: /languages
+ */
+
+function topher_get_wptv_data() {
+
+	// Get a SimplePie feed object from the specified feed source.
+	$rss = fetch_feed( 'https://wordpress.tv/speakers/topher-derosia/feed/' );
+	 
+	$maxitems = 0;
+	 
+	if ( ! is_wp_error( $rss ) ) {
+	 
+		// Figure out how many total items there are, but limit it to 5. 
+		$maxitems = $rss->get_item_quantity( 500 ); 
+	 
+		// Build an array of all the items, starting with element 0 (first element).
+		$rss_items = $rss->get_items( 0, $maxitems );
+	 
+	}
+
+	$data = [];
+
+	foreach( $rss_items as $key => $item ) {
+
+		$enclosures = $item->get_enclosures();
+
+		$data[ $key ]['publishedAt'] = $item->get_date();
+		$data[ $key ]['title']       = $item->get_title();
+		$data[ $key ]['description'] = $item->get_description();
+		$data[ $key ]['link']        = $enclosures[0]->link;
+		$data[ $key ]['image']       = $enclosures[0]->thumbnails[0];
+		$data[ $key ]['videoId']     = $item->get_id();
+	}
+
+	return $data;
+}
+add_shortcode( 'topher_get_wptv_data', 'topher_get_wptv_data' );
+
+function topher_get_wptv_videoids() {
+
+	global $wpdb;
+
+	$query   = "SELECT DISTINCT `meta_value`  FROM `baf_postmeta` WHERE `meta_key` = 'videoID'";
+
+	$results = $wpdb->get_col( $query );
+
+	return $results;
+
+}
+
+function topher_save_wptv_data() {
+
+	if ( is_user_logged_in() ) {
+		return;
+	}
+
+	if( empty( $_GET['wptvimport'] ) || $_GET['wptvimport'] != 'yes' ) {
+		return;
+	}
+
+	$data = topher_get_wptv_data();
+
+	$keys = topher_get_wptv_videoids();
+
+	foreach( $data as $video_data ) {
+
+		// check to see if we already have this one.
+		if ( in_array( $video_data['videoId'], $keys ) ) {
+			continue;
+		}
+
+		$body = '[video src="' . $video_data['link'] . '" width="1280"]' . "\n";
+		$body .= $video_data['description'];
+
+		$post = [
+			'post_author'    => 'topher',
+			'post_date_gmt'  => $video_data['publishedAt'],
+			'post_content'   => $body,
+			'post_title'     => $video_data['title'],
+			'post_status'    => 'publish',
+			'post_type'      => 'post',
+			'comment_status' => 'closed',
+			'ping_status'    => 'closed',
+			'post_category'  => [ 2, 3 ],
+			'meta_input'     => [ 'videoId' => $video_data['videoId'] ],
+		];
+
+		$post_id = wp_insert_post( $post );
+
+		require_once(ABSPATH . 'wp-admin/includes/media.php');
+		require_once(ABSPATH . 'wp-admin/includes/file.php');
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
+
+		$attachment_id = media_sideload_image( $video_data['image'], $post_id, 'Image for ' . $video_data['title'], 'id' );
+
+		set_post_thumbnail($post_id, $attachment_id );
+
+	}
+}
+add_action( 'init', 'topher_save_wptv_data' );
